e0ebcb0813d1c8962bfc21c73702c928b036bf27
teshank2137 <teshank.2137@gmail.com>
2022-11-21 22:13:51 +0530

    bg text added

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import { typeScale } from "../utils";
 import { Grid, PageHeader } from "./ui";
 
 const StyledLayout = styled(Grid)`
   height: 100vh;
   padding-top: 120px;
   grid-template-rows: min-content auto;
+  position: relative;
+  overflow: hidden;
+  .bg-text {
+    position: absolute;
+    top: 30%;
+    font-size: 8rem;
+    opacity: 0.2;
+    font-weight: 800;
+    right: 0;
+    z-index: -10;
+    transform: translateX(30%);
+  }
 `;
 
 export const Page = ({ children, header }) => {
@@ -15,6 +28,7 @@ export const Page = ({ children, header }) => {
         <PageHeader>{header}</PageHeader>
         {children}
       </>
+      <div className="bg-text">{header}</div>
     </StyledLayout>
   );
 };
